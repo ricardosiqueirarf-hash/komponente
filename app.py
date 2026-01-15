06a72d8a190c3c1a999907470a3c199f63a45e64
lowkanta <ricardosiqueirarf@gmail.com>
2026-01-15 17:45:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='.')
@@ -15,4 +17,7 @@ def cadastro():
     return render_template("cadastro.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("FLASK_DEBUG", "").lower() in {"1", "true", "yes"}
+    app.run(host="0.0.0.0", port=port, debug=debug)
+
