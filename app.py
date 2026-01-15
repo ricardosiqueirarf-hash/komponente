3e9717ab0cea9f147b70cf559b07cdb02fb802bf
lowkanta <ricardosiqueirarf@gmail.com>
2026-01-15 15:47:53 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='.')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/buscar")
+def buscar():
+    return render_template("buscar.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
